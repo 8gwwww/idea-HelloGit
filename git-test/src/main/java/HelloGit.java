94d3c011ae62d0be0c6c6a2ed2e217df20a31109
wilbergerrard <2691495848@qq.com>
2020-11-25 11:14:12 +0800

    创建分支1

--- git-test/src/main/java/HelloGit.java
+++ git-test/src/main/java/HelloGit.java
@@ -2,6 +2,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("好好学习");
+        System.out.println("dev1");
 
     }
 }
